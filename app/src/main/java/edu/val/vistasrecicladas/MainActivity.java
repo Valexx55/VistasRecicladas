56948596ad1fc9c3de043f6f1a3ffe7a4057954c
valexx55 <valexx55@gmail.com>
2022-06-27 18:46:25 +0200

    searchview 1/2

--- app/src/main/java/edu/val/vistasrecicladas/MainActivity.java
+++ app/src/main/java/edu/val/vistasrecicladas/MainActivity.java
@@ -6,22 +6,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private RecyclerView recyclerView;//es el padre de la lista de datos - el contendor
     private static final int TAMANIO_LISTA_LIBROS = 100;
+    private SearchView searchView;
+    private List<Libro> libroList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.searchView = findViewById(R.id.searchView);
         this.recyclerView = findViewById(R.id.recicler_view);
-        List<Libro> libroList = generarListaLibros();
+        libroList = generarListaLibros();
 
         AdapterListaLibros adapterListaLibros = new AdapterListaLibros(libroList);
 
@@ -29,9 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         //ahora definimos el estilo
         RecyclerView.LayoutManager layoutManager =  new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-         this.recyclerView.setLayoutManager(layoutManager);
+         //this.recyclerView.setLayoutManager(layoutManager);
+        this.recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
 
          //echad un vistazo al  StaggeredGridLayoutManager para distrbucion en celdas
+        this.searchView.setOnQueryTextListener(this);
     }
 
 
@@ -50,5 +57,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         return lista_libros;
+    }
+
+    //lista_filtrada filtrarLista (lista, busqueda_usuario)
+    private List<Libro> filtrarLista (List<Libro> lista_libros, String busqueda_usuario)
+    {
+        List<Libro> lista_libros_filtrada = null;
+
+            //recorrer con un for
+                //si la busqueda de usuario, coincide con el elemento de la lista
+                    //a la buchaca, lo meto en la nueva lista
+               //si no --> no hago nada
+
+        return lista_libros_filtrada;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String termino_busqueda) {
+        //método invocado al realizar la búsqueda
+        Log.d("ETIQUETA_LOG", "onQueryTextSubmit = " + termino_busqueda);
+        //FILTRAR POR LA CADENA DE BÚSQUEDA S
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        //método invocado al modficar el contenido de la caja de búsqueda
+        Log.d("ETIQUETA_LOG", "onQueryTextChange = " + s);
+        return true;
     }
 }
